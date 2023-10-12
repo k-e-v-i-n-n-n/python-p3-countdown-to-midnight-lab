080f74355e248ba36024773659b44b3e8d95c779
Kevin Breid <kevinbreid@yahoo.com>
2023-10-12 13:42:42 +0200

    practicing python loops

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,19 @@
 # your code goes here!
+import time
+
+
+def countdown(num):
+    # count = num
+    while num > 0:
+        format = f'{num} SECOND(S)!'
+        print(format)
+        num -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(num):
+    while num > 0:
+        format = f'{num} SECOND(S)!'
+        print(format)
+        num -= 1
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
